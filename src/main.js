049e3a0f7283fabd917477c487e38e94119d70ce
Kylart <Kylart.dev@gmail.com>
2019-06-29 01:19:48 +0200

    Removed serviceworker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
